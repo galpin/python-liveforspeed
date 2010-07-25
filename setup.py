c96129baf8c901d096a62313364fbfe3e5d90654
Martin Galpin <m@66laps.com>
2010-07-25 16:47:42 +0100

    Creates setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+  name='python-liveforspeed',
+  version='1.0',
+  description='A library that provides a python interface to the Live For Speed Replay Analyser Format (RAF).',
+  author='Martin Galpin',
+  author_email='m@66laps.com',
+  url='http://developer.66laps.com/',
+  modules=['lfs']
+)
